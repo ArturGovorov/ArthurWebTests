84e93d1731ec7e2f7d29026a0df744e03b771378
ArturGovorov <ArturGovorov@users.noreply.github.com>
2025-05-17 21:39:45 +0300

    Правки по локаторам

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -3,16 +3,18 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPageLocators:
+    LOGIN_TAB = (By.XPATH, '//*[data-l="t,login_tab"]')
+    QR_TAB = (By.XPATH, '//*[data-l="t,qr_tab"]')
     LOGIN_FIELD = (By.ID, 'field_email')
+    PASSWORD_FIELD = (By.ID, 'password')
     LOGIN_BUTTON = (By.XPATH, '//*[@data-l="t,sign_in"]')
-    PASSWORD = (By.XPATH, '//*[@data-l="t,password"]')
-    QR_CODE = (By.ID, 'qrCode-4467669758')
-    QR_CODE_BUTTON = (By.XPATH, '//*[@data-l="t,get_qr"]')
-    LOGIN_ERROR = (By.XPATH, '//*[@data-l="t,restore"]')
-    REGISTER = (By.XPATH, '//*[@data-l="t,register"]')
-    LOGIN_VIA_VK = (By.ID, 'tabpanel-login-4467669716')
-    LOGIN_VIA_MAIL = (By.ID, 'tabpanel-login-4467669716')
-    LOGIN_VIA_YANDEX = (By.ID, 'tabpanel-login-4467669716')
+    LOGIN_BY_QR_CODE = (By.XPATH, '//*[@data-l="t,get_qr"]')
+    RESTORE_LINK = (By.XPATH, '//*[@data-l="t,restore"]')
+    REGISTRATION_BUTTON = (By.XPATH, '//*[<div class="external-oauth-login-footer"]/a[@data-l="t,register"]')
+    VK_BUTTON = (By.XPATH, '//*[@data-l="t,vkc"]')
+    MAIL_BUTTON = (By.XPATH, '//*[@data-l="t,mailru"]')
+    YANDEX_BUTTON = (By.XPATH, '//*[@data-l="t,yandex"]')
+    OTHER_BUTTON = (By.XPATH, '//*[@data-l="t,other"]')
 
 
 
