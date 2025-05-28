f13dec2ae430a27650d06d3d4b4ccd6cf3af0449
ArturGovorov <ArturGovorov@users.noreply.github.com>
2025-05-29 00:13:05 +0300

    Написал тест для проверки корректности отображения страницы при переходе в рекламный кабинет

---
+++ tests/HelpTests.py
@@ -0,0 +1,17 @@
+import allure
+
+from core.BaseTest import browser
+from pages.BasePage import BasePage
+from pages.HelpPage import HelpPageLocators, HelpPageHelper
+from pages.AdvertisementCabinetHelp import AdvertisementCabinetHelpHelper
+
+BASE_URL = 'https://ok.ru/help'
+
+
+@allure.suite('Проверка формы помощь')
+@allure.title('Проверка корректности отображения страницы при переходе в рекламный кабинет')
+def test_help_test(browser):
+    BasePage(browser).get_url(BASE_URL)
+    HelpPage = HelpPageHelper(browser)
+    HelpPage.scrollToitem(HelpPageLocators.ADVERTISEMENT_CABINET)
+    AdvertisementCabinetHelpHelper(browser)
